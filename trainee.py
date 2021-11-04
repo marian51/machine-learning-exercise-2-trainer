844f93c5f8053ae3cac0714820e6b6022f806481
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2021-11-04 23:02:50 +0100

    load old trainee file

--- trainee.py
+++ trainee.py
@@ -2,45 +2,52 @@ import argparse
 import fileinput
 import sys
 
-#parsowanie argumentow
 parser = argparse.ArgumentParser(description="Do")
 parser.add_argument('-d', '--description')
 args = parser.parse_args()
 
-array = [] # tablica na dane z pliku description
+#print("dziala")
+#print(args.description)
 
-# przerobienie danych z pliku na tablice dwuwymiarowa
+array = []
+
 for line in fileinput.input(files =args.description):
     line = line.rstrip()
     line = line.split()
     array.append(line)
 
+
 #odczytanie pliku in.txt
 file_in = sys.stdin.read()
-
 #przerobienie na tablice dwuwymiarowa
 file_in = file_in.split('\n')
+
 for i in range(len(file_in)):
     file_in[i] = file_in[i].split()
     file_in[i].insert(0,1)
    
-result = 0 # koncowy wynik danego rownania
-equation = 0 # wartosc podstawiana pod dany x
+#print(file_in) # tablica tablic
+#print(array)
+result = 0
+equation = 0
 
-dimension = int(array[0][1]) # stopien wielomianu
+dimension = int(array[0][1])
+#print(dimension)
+#print("n petla")
 
-# glowna czesc programu 
-for k in range(len(file_in)-1): # petla iterujaca po wierszach in.txt
-    equation = file_in[k] # stopien danego wielomianu
-    result = 0 # wyzerowanie koncowego wyniku przed przejsciem do nastepnej iteracji
-    for i in range(1, len(array)): # petla iterujaca po skladnikach sumy (koncowego wyniku); od 1, bo 1 wiersz description to inne dane niz reszta
-        line = array[i] # dany wiersz pliku description
-        factor = float(line[-1]) # wspolczynnik (ostatni element wiersza)
-        inner_result = factor  # wynik danego skladnika sumy
-        for j in range(dimension): # petla iterujaca po danym elemencie sumy; przemnaza wspolczynnik przez dane wartosci danych x
+for k in range(len(file_in)-1):
+    equation = file_in[k]
+    result = 0
+    for i in range(1, len(array)):
+        line = array[i]
+        factor = float(line[-1])
+        inner_result = factor
+        #print("\n",factor)
+        for j in range(dimension):
 	        inner_result *=  float(equation[int(line[j])])
 		
-        result += inner_result # sumowanie skladnikow wielomianu
+        #print(inner_result)
+        result += inner_result
 	
-    print(result) # zwrocenie koncowego elementu
+    print(result)
 
